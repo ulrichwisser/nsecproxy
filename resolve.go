f4923a77b2ee3a9850042e2da9c9d56ac19054ee
Ulrich Wisser <ulrich@wisser.se>
2020-09-14 13:17:13 +0200

    match to canonical name

--- resolve.go
+++ resolve.go
@@ -12,7 +12,7 @@ func resolve(config *Configuration, w dns.ResponseWriter, req *dns.Msg) {
 	qname := req.Question[0].Name
 
 	re := regexp.MustCompile("nsec\\d")
-	switch re.FindString(qname) {
+	switch re.FindString(dns.CanonicalName(qname)) {
 	case "nsec1":
 		log.Printf("routing NSEC    request %s to       %s\n", qname, config.UpstreamNSEC)
 		resolveUpstream(config.UpstreamNSEC, w, req)
@@ -22,6 +22,9 @@ func resolve(config *Configuration, w dns.ResponseWriter, req *dns.Msg) {
 	case "nsec4":
 		log.Printf("routing NSEC4   request %s\n", qname)
 		resolveNSEC4(config, w, req)
+	case "nsec5":
+		log.Printf("routing NSEC5   request %s\n", qname)
+		resolveNSEC5(config, w, req)
 	default:
 		if rand.Intn(2) < 1 {
 			log.Printf("routing DEFAULT request %s to NSEC  %s\n", qname, config.UpstreamNSEC)
@@ -65,6 +68,26 @@ func resolveNSEC4(config *Configuration, w dns.ResponseWriter, req *dns.Msg) {
 	w.WriteMsg(resp)
 }
 
+func resolveNSEC5(config *Configuration, w dns.ResponseWriter, req *dns.Msg) {
+	resp1 := getAnswer(config.UpstreamNSEC, req)
+	resp3 := getAnswer(config.UpstreamNSEC3, req)
+
+	resp := resp3.Copy()
+	for _, rr := range resp1.Ns {
+		log.Printf("Adding to resp: %s\n", rr.String())
+		if rr.Header().Rrtype == dns.TypeSOA {
+			continue
+		}
+		if rr.Header().Rrtype == dns.TypeRRSIG && rr.(*dns.RRSIG).TypeCovered == dns.TypeSOA {
+			continue
+		}
+		resp.Ns = append(resp.Ns, rr)
+	}
+
+	log.Println(resp.String())
+
+	w.WriteMsg(resp)
+}
 func getAnswer(upstream string, req *dns.Msg) *dns.Msg {
 	c := &dns.Client{Net: "udp"}
 	resp, _, err := c.Exchange(req, upstream)
